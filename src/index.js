7738678eb670247fb5f042120b91ab73a701dd09
Casey Baker <cj-baker@live.com>
2023-05-16 10:17:42 -0700

    Added JS for Current Day and Time

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-
+let now = new Date();
+let currentDay = document.querySelector("div.current-day");
+let currentTime = document.querySelector("div.current-time");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+currentDay.innerHTML = day;
+currentTime.innerHTML = now.toLocaleTimeString([], {
+  hour: "2-digit",
+  minute: "2-digit",
+});
